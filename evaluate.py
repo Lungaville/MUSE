fb29a37d5218f737075e356cf71f78a006b08ca3
Lungavile <lungavile@gmail.com>
2021-05-05 07:45:55 +0700

    hotfix bugs

--- evaluate.py
+++ evaluate.py
@@ -71,6 +71,7 @@ log_export['csls-5'] = to_log['precision_at_5-csls_knn_10']
 log_export['csls-10'] = to_log['precision_at_10-csls_knn_10']
 log_export['mean-cosine-nn']= to_log['mean_cosine-nn-S2T-10000']
 log_export['mean-cosine-csls']= to_log['mean_cosine-csls_knn_10-S2T-10000']
+print(params.model_name + ' Result')
 print(log_export)
 df = pd.DataFrame([log_export])
 df.to_csv(f"/kaggle/working/{params.model_name}.csv")
